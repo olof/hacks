3042b7871dbe5b85abe60cf39ba2946e56d88283
Olof Johansson <olof@ethup.se>
2011-02-13 00:49:55 +0100

    Added support for multiple services
    Also changed license to GNU APL.

--- irssi/tinyurl-resolver.pl
+++ irssi/tinyurl-resolver.pl
@@ -1,54 +1,98 @@
 #!/usr/bin/perl
+# Copyright 2009-2011, Olof Johansson <olof@ethup.se>
+#
+# Copying and distribution of this file, with or without 
+# modification, are permitted in any medium without royalty 
+# provided the copyright notice are preserved. This file is 
+# offered as-is, without any warranty.
+
 use strict;
 use IO::Socket::INET;
 use Irssi;
 
-my $VERSION = '0.3';
+my $VERSION = '0.4';
 my %IRSSI = (
     authors     => 'Olof \'zibri\' Johansson',
     contact     => 'olof@ethup.se',
     name        => 'tinyurl-resolver',
-    description => 'resolve tinyurl',
-    license     => 'GPLv2 or later',
+    description => 'resolve tinyurl, et al',
+    license     => 'GNU APL',
 );
 
-# modified version of youtube-title.pl 
-# http://www.stdlib.se/
+# 2011-02-13, 0.4:
+# * added support for multiple url shortening services
+# * changed license to GNU APL
+
+# This started of as a modified version of youtube-title.pl 
+# See also:
+# * http://www.stdlib.se/
+# * https://github.com/olof/hacks/tree/master/irssi
 
 my $debug = 1;
+my $prefix = qr,(?:http://(?:www\.)?|www\.),;
+my @tinyfiers = (
+	qr,${prefix}tinyurl\.com/\w+,,
+	qr,${prefix}bit\.ly/\w+,,
+	qr,${prefix}cot\.ag/\w+,,
+	qr,${prefix}ow\.ly/\w+,,
+);
+
+sub hastiny {
+	my($msg) = @_;
+	print $msg;
+
+	foreach(@tinyfiers) {
+		if(my($url) = $msg =~ /($_)/) {
+			if($url =~ /^www/) {
+				return "http://$url";
+			}
+
+			return $url;
+		}
+	}
+
+	return undef;
+}
 
 sub istiny {
 	my($server, $msg, $nick, $address, $target) = @_;
 	$target=$nick if $target eq undef;
 
-	while($msg=~ /(?:http:\/\/(?:www\.)?|(?:www\.)?)tinyurl\.com\/(\w+)/) {
-		my $loc = get_location($1) if($1 ne undef);
+	while(my $url = hastiny($msg)) {
+		my $loc = get_location($url);
 		
-		if($loc ne undef) {
-			$server->window_item_find($target)->
-				print("%ytinyurl.com%n/$1 -> $loc", MSGLEVEL_CLIENTCRAP);
-		} else {
-			$server->window_item_find($target)->
-				print("%ytinyurl:%n invalid tinyurl link", MSGLEVEL_CLIENTCRAP);
+		if($loc) {
+			$server->window_item_find($target)->print(
+				"%y$url%n/$1 -> $loc", 
+				MSGLEVEL_CLIENTCRAP
+			);
+		} elsif($debug) {
+			$server->window_item_find($target)->print(
+				"%y$url:%n invalid link", 
+				MSGLEVEL_CLIENTCRAP
+			);
 		}
 
-		$msg =~ s/(?:http:\/\/(?:www\.)?|(?:www\.)?)tinyurl\.com\/\w+//;
+		$msg =~ s/$url//;
 	}
 }
 
 sub get_location {
 	my $location;
-	my ($id) = @_;
+	my ($url) = @_;
 	
+	my ($host) = $url =~ m,http://(.[^/:]+)/,;
+	return undef unless defined $host;
+
 	my $sock = IO::Socket::INET->new (
-		PeerAddr => 'tinyurl.com',
+		PeerAddr => $host,
 		PeerPort => 'http(80)',
 		Proto => 'tcp',
 	);
 	
-	print $sock "GET /$id HTTP/1.1\r\n".
-	            "host: tinyurl.com\r\n".
-	            "user-agent: anti-tinyurl/0.1\r\n".
+	print $sock "GET $url HTTP/1.1\r\n".
+	            "host: $host\r\n".
+	            "user-agent: tinyurl-resolver/0.4 (irssi)\r\n".
 	            "\r\n";
 	
 	while(<$sock>) {
