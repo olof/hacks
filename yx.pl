fba5105fd699c21df2566cbe5057c33a41a88fa9
Olof Johansson <olof@ethup.se>
2011-08-13 12:31:11 +0200

    Adds support for downloading videos

--- yx.pl
+++ yx.pl
@@ -1,14 +1,18 @@
 #!/usr/bin/perl
 # Youtube streamer / downloader
-# Deps: URI::Escape, LWP::Simple, mplayer
-#
-# Copyright (c) 2011 - Olof Johansson <olof@ethup.se>
-# All rights reserved.
 # 
-# This program is free software; you can redistribute it and/or 
-# modify it under the same terms as Perl itself.
+# Common-Deps: URI::Escape, LWP::Simple
+# Streaming-Deps: mplayer
+# Download-Deps: HTML::Entities, curl, wget
+#
+# Copyright 2011, Olof Johansson <olof@ethup.se>
+#
+# Copying and distribution of this file, with or without 
+# modification, are permitted in any medium without royalty 
+# provided the copyright notice are preserved. This file is 
+# offered as-is, without any warranty.
 
-our $VERSION = 3.141;
+our $VERSION = 3.1415;
 use warnings;
 use strict;
 use LWP::Simple qw/get/;
@@ -16,9 +20,26 @@ use URI::Escape;
 use Data::Dumper;
 use Getopt::Long;
 
+sub get_filename {
+	require HTML::Entities;
+	HTML::Entities->import;
+	my $hash = shift;
+	my $htmlr = shift;
+	my ($_) = $$htmlr =~ /<title>\s*(.*?)\s*-\s*YouTube\s*<\/title>/s;
+
+	$_ = decode_entities($_);
+	s/(?<=\s)&(?=\s)/and/g;
+	s/ +/_/g;
+	s/[^\w_]//g;
+	
+	return lc $_ . '.' . $hash->{ext};
+}
+
 my @pref = qw/45 44 43 18/;
 my $opts = {};
-GetOptions($opts, 'itag=i', 'dumper');
+GetOptions($opts, 'itag=i', 'stream', 'download', 'curl', 'wget',  'dumper|D');
+$opts->{curl} = 1 if $opts->{download};
+$opts->{stream} = 1 unless $opts->{curl} || $opts->{wget};
 
 @ARGV > 0 || die('needs an uri as argument');
 my $html = get($ARGV[0]);
@@ -30,14 +51,25 @@ my %map = map {
 		} split /&/, $_
 	}; 
 	$tmp->{url} = uri_unescape($tmp->{url});
+	$tmp->{type} = uri_unescape($tmp->{type});
+	my $_ = shift @{[split /;/, $tmp->{type}]};
+	s;.*/(?:x-)?;;;
+	$tmp->{ext} = $_;
 	($tmp->{itag} => $tmp)
 } split/,/,uri_unescape($map);
 
 $opts->{dumper} && do { print Dumper(\%map); exit 0 };
 
-my $uri;
-$uri = $map{$opts->{itag}}->{url};
-$uri = $map{$_}->{url} while(!$uri && ($_ = shift @pref));
-die("couldn't find uri...") unless $uri;
-system("mplayer -fs '$uri'");
+my $itag;
+$itag = $opts->{itag} if $opts->{itag} and $map{$opts->{itag}}->{uri};
+$itag = $map{$_}->{url} ? $_ : undef while(!$itag && ($_ = shift @pref));
+die("couldn't find uri...") unless $itag;
+
+my $uri = $map{$itag}->{url};
+exec("mplayer -fs '$uri'") if $opts->{stream};
+
+my $fname = get_filename($map{$itag}, \$html);
+print "Downloading to $fname\n";
+exec("curl -Lo $fname '$uri'\n") if $opts->{curl};
+exec("wget -O $fname '$uri'") if $opts->{wget};
 
