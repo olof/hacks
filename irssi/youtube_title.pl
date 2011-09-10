10a5bc4ed28ed95b67c39d663415e3179509df34
Olof Johansson <olof@ethup.se>
2011-09-10 19:11:23 +0200

    youtube_title: fix whitespace damage in changelog

--- irssi/youtube_title.pl
+++ irssi/youtube_title.pl
@@ -19,46 +19,46 @@ my $VERSION = '0.51';
 
 # changelog:
 # 0.51, 2011-09-05:
-#       * Bugfix release: "<url>," will fail (note the , character)
+#   * Bugfix release: "<url>," will fail (note the , character)
 # 0.5, 2011-06-02:
-#       * Optionally print info of own Youtube links (defaults to off)
-#         (see setting "yt_print_own").
+#   * Optionally print info of own Youtube links (defaults to off)
+#     (see setting "yt_print_own").
 # 0.4, 2011-05-23:
-#       * Also print out duration of video
-#       * Broke out output code to subroutines
-#       * Respect $PROXY envs
-#       * Generally replaced hacks with available modules
-#         - Replaced IO::Socket::INET with LWP::UserAgent
-#         - Replaced regexp for XML parsing with XML::Simple
-#         - Replaced regexp for HTML entity decoding with HTML::Entities
-#         - Replaced URI parsing with regexp with URI.pm and Regexp::Common
+#   * Also print out duration of video
+#   * Broke out output code to subroutines
+#   * Respect $PROXY envs
+#   * Generally replaced hacks with available modules
+#     - Replaced IO::Socket::INET with LWP::UserAgent
+#     - Replaced regexp for XML parsing with XML::Simple
+#     - Replaced regexp for HTML entity decoding with HTML::Entities
+#     - Replaced URI parsing with regexp with URI.pm and Regexp::Common
 # 0.32, 2010-06-05:
-#	* added license header
-#	* updated contact info
-#	* minor formatting
+#   * added license header
+#   * updated contact info
+#   * minor formatting
 # 0.31, 2009-10-15:
-#	* formatting...
+#   * formatting...
 # 0.3:
-# 	* set the message level to CLIENTCRAP and put the text 
-#         "youtube:" in yellow colour. 
+#   * set the message level to CLIENTCRAP and put the text 
+#     "youtube:" in yellow colour. 
 # 0.21:
-#	* whoa, fucked up bug. 
-#         http://www.youtube.com/watch?hl=en&v=tqZiY2RdoO4&gl=US 
-#         was posted to a channel, and my script got fucked up. 
-#         it printed the title of the video ~5 times a second, 
-#         until i checked irssi and had to kill the whole client.
-#	  	Anyways, it could have to do with the weird order 
-#         of the query string, which is not to say that it's not 
-#         my fault - 1. i should handle it, and 2. i should not 
-#         fuck up irssi. What I did wrong was to check for one 
-#         regexp, and when found remove another regexp - i should 
-#         have deleted the same regexp, which i do now. 
+#   * whoa, fucked up bug. 
+#     http://www.youtube.com/watch?hl=en&v=tqZiY2RdoO4&gl=US 
+#     was posted to a channel, and my script got fucked up. 
+#     it printed the title of the video ~5 times a second, 
+#     until i checked irssi and had to kill the whole client.
+#     	Anyways, it could have to do with the weird order 
+#     of the query string, which is not to say that it's not 
+#     my fault - 1. i should handle it, and 2. i should not 
+#     fuck up irssi. What I did wrong was to check for one 
+#     regexp, and when found remove another regexp - i should 
+#     have deleted the same regexp, which i do now. 
 #
 # 0.2:
-#	* regexp didn't match all youtube videos. if you have a 
-#         variable in the query string before the v, it will not 
-#         find it. And i also found out that all vs isn't 11 
-#         characters.
+#   * regexp didn't match all youtube videos. if you have a 
+#     variable in the query string before the v, it will not 
+#     find it. And i also found out that all vs isn't 11 
+#     characters.
 
 my %IRSSI = (
 	authors     => 'Olof \'zibri\' Johansson',
