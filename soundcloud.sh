ea8ff62430d325c9865a0679a12eae44b0fe6837
Olof Johansson <olof@ethup.se>
2012-04-14 12:51:20 +0200

    soundcloud.sh: download streams from soundcloud

---
+++ soundcloud.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Copyright 2011, Olof Johansson <olof@ethup.se>
+#
+# Copying and distribution of this file, with or without 
+# modification, are permitted in any medium without royalty 
+# provided the copyright notice are preserved. This file is 
+# offered as-is, without any warranty.
+
+# Depends on curl
+
+die() {
+	echo Error: "$@" >&2
+	exit 1
+}
+
+WEB=$1
+[ -n "$WEB" ] || die 'Need to supply a URL'
+TITLE="${WEB##*/}"
+
+[ -n "$TITLE" ] || die "$WEB should not end with /"
+
+STREAM=$(
+	curl -s "$WEB" | grep -F "$TITLE" | 
+		sed -ne 's/.*"streamUrl":"\([^"]\+\).*/\1/p'
+)
+[ -n "$STREAM" ] || die "Unable to find stream url in $WEB"
+
+curl -Lo "$TITLE.mp3" "$STREAM"
