a6c7745b3ad4c435b15fc21dfe03f98ffed1448d
Olof Johansson <olof@ethup.se>
2012-10-13 23:21:55 +0200

    hls-hack: download HLS streams

---
+++ hls-hack.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+die() { echo "Error:" "$@" >&2; exit 1; }
+
+URL=$1
+OUT=$2
+[ "$URL" ] || die "No url supplied"
+[ "$OUT" ] || die "No output filename supplied"
+[ ! -e "$OUT" ] || die "Output file already exists"
+
+M3U=$(mktemp /tmp/hls-hack.m3u8-XXXXXX)
+[ -e "$M3U" ] || die "Could not create temp file"
+
+curl -s "$URL" | grep ^http >$M3U
+
+COUNT=$(wc -l <$M3U)
+[ "$COUNT" -gt 0 ] || die "Not a valid m3u file"
+
+N=1
+while read url; do
+	echo "Downloading chunk $N/$COUNT"
+	curl "$url" >>"$OUT"
+	N=$((N+1))
+done <$M3U
